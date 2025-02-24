1ced20126b4a1db6233e4791323f4498fc1d95af
responderi <responderi@users.noreply.github.com>
2025-02-24 17:05:56 +0200

    Initial commit - MBP Discord AI Bot

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using OpenAI;
+using dotenv.net;
+
+class Program
+{
+    private static DiscordSocketClient _client;
+    private static OpenAIClient _openAi;
+}
